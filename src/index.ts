68ffbb67b4fd1ee9fe88330f67ff6eea8985aeb8
Banja David Lotigo <innocdavid@gmail.com>
2024-06-04 12:10:11 +0300

    add database connection

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import 'dotenv/config';
+
+//db
+mongoose.connect(process.env.MANGODB_URL as string)
+    .then(() => {
+        console.log('Conected to database');
+    });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.get("/api/test", async (req: Request, res: Response) => {
+app.get("/api/v1/test", async (req: Request, res: Response) => {
     res.status(201).json({ message: "Hello from the server"});
 });
 
 app.listen(8080, () => {
-    console.log('server listening at 127.0.0.1:8080');
+    console.log('Server listening at localhost:8080');
 });
