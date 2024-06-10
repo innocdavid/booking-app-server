c8dd9a78eb6e569d27f59dfdc87964b8079733e7
Banja David Lotigo <innocdavid@gmail.com>
2024-06-10 11:43:06 +0300

    update endpoint

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use("/api/vi/users", userRouter)
+app.use("/api/v1/users", userRouter)
 
 app.listen(8080, () => {
     console.log('Server listening at localhost:8080');
