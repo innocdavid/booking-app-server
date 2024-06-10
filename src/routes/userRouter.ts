d34c6a2237b9c97c961ae7b50650d05fb9a4f2fa
Banja David Lotigo <innocdavid@gmail.com>
2024-06-10 11:42:36 +0300

    add validation to register route

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import userController from "../controllers/userController";
+import { validateUser } from "../middlewares/validate";
 
 const router = express.Router();
 
-router.post("/register", userController.createUser);
+router.post("/register", validateUser, userController.createUser);
 
 export default router;
