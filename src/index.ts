b7c606c3ee39b47930b2c319fcd08b9786fe0fa4
Banja David Lotigo <innocdavid@gmail.com>
2024-06-10 12:48:32 +0300

    add auth route and refactor index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import express, { Request, Response } from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import 'dotenv/config';
-import userRouter from './routes/userRouter';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import "dotenv/config";
+import authRoutes from "./routes/authRouter";
+import userRoutes from "./routes/userRouter";
 
 //db
 mongoose.connect(process.env.MANGODB_URL as string)
     .then(() => {
-        console.log('Conected to database');
+        console.log("Conected to database");
     });
 
 const app = express();
@@ -15,8 +16,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use("/api/v1/users", userRouter)
+// routes
+app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/users", userRoutes)
 
+// server listening
 app.listen(8080, () => {
-    console.log('Server listening at localhost:8080');
+    console.log("Server listening at localhost:8080");
 });
