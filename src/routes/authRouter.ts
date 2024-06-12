0693b8226153226e86a394f131a0ce16a4418f41
Banja David Lotigo <innocdavid@gmail.com>
2024-06-12 12:41:57 +0300

    verify token route

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import authController from "../controllers/authController";
 import { validateLogin } from "../middlewares/validate";
+import verifyAuthToken from "../middlewares/auth";
 
 const router = express.Router();
 
 router.post("/login", validateLogin, authController.loginAuth);
+
+router.get("/validate-token", verifyAuthToken, (req: Request, res: Response) => {
+    res.status(200).send({ userId: req.userId });
+})
 
 export default router;
