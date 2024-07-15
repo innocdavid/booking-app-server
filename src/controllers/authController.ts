5cd5fa2e95b0019dbb7f03b335c6f004551a8147
Banja David Lotigo <innocdavid@gmail.com>
2024-07-15 12:41:39 +0300

    update cookie parameters

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -31,6 +31,7 @@ const loginAuth = async (req: Request, res: Response) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             maxAge: 86400000,
+            sameSite: 'none',
         });
 
         res.status(200).json({ userId: user._id });
