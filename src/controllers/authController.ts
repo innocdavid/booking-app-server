46cb70e051217432ab433fd9e345ad35ed3eec44
Banja David Lotigo <innocdavid@gmail.com>
2024-07-15 12:48:24 +0300

    fix prod bug

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -31,7 +31,7 @@ const loginAuth = async (req: Request, res: Response) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             maxAge: 86400000,
-            sameSite: 'none',
+            sameSite: 'strict',
         });
 
         res.status(200).json({ userId: user._id });
